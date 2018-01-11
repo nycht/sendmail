20141b7b15cab458181ff91c003f11aa20fedd7c
nycht <552010694@qq.com>
2018-01-11 09:37:27 +0800

    qimo

---
+++ page3.py
@@ -0,0 +1,38 @@
+#-*- coding:utf-8 –*-
+import urllib
+import urllib2
+import re
+import thread
+import time
+
+class caihaopage3:
+    def __init__(self):
+        self.user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+        self.pageStories = []
+        self.url = 'http://www.caihao.com/job/all/3'
+        self.headers = {'User-Agent': self.user_agent}
+
+    def getpage(self, index):
+        user_agant = self.user_agent
+        url = self.url
+        headers = {'User-Agent': user_agant}
+        result = urllib2.Request( url, headers=headers )
+        response = urllib2.urlopen( result )
+        pageCode = response.read().decode( 'utf-8' )
+        return pageCode
+
+    def pageindex(self, index):
+        pageCode = self.getpage( index )
+        pattern = re.compile(r'<li class="zbn2 bold font14">.*?<a.*?>(.*?)</a>.*?</li>.*?<li class="zbn3".*?<a.*?>(.*?)</a>.*?</li>.*?<li class="zbn4">(.*?)&nbsp;</li>.*?<li>(.*?)</li>',re.S )
+        items = re.findall( pattern, pageCode )
+        for i in items:
+            input = raw_input()
+            for g in i:
+                print g
+
+    def start(self):
+        print "--------------------------------------page3------------------------------------"
+        print "-------------             i'm    Separation   line         --------------------"
+        self.getpage(index=caihaopage3)
+        self.pageindex(index=caihaopage3)
+
---
+++ start.py
@@ -0,0 +1,12 @@
+from page1 import caihaopage1
+from page2 import caihaopage2
+from page3 import caihaopage3
+from page4 import caihaopage4
+a = caihaopage1()
+a.start()
+b = caihaopage2()
+b.start()
+c=caihaopage3()
+c.start()
+d = caihaopage4()
+d .start()
